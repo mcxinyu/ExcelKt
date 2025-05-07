38a9c8250bbfc212a35ca9d3db0842ec4042137d
HuangYueFeng(njf) <mcxinyu@foxmail.com>
2025-05-07 10:04:03 +0800

    update poi

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,9 @@ repositories {
 
 dependencies {
     // Apache POI Excel library
-    api("org.apache.poi:poi-ooxml:5.2.2")
-    implementation("org.apache.logging.log4j:log4j-api:2.17.2")
-    implementation("org.apache.logging.log4j:log4j-core:2.17.2")
+    api("org.apache.poi:poi-ooxml:5.4.0")
+    implementation("org.apache.logging.log4j:log4j-api:2.24.3")
+    implementation("org.apache.logging.log4j:log4j-core:2.24.3")
 
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
@@ -90,24 +90,24 @@ publishing {
         }
     }
 
-    repositories {
-        maven {
-            val releaseUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots"
-
-            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotUrl else releaseUrl)
-
-            credentials {
-                val ossrhUsername: String by project
-                val ossrhPassword: String by project
-
-                username = ossrhUsername
-                password = ossrhPassword
-            }
-        }
-    }
+    // repositories {
+    //     maven {
+    //         val releaseUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+    //         val snapshotUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots"
+    //
+    //         url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotUrl else releaseUrl)
+    //
+    //         credentials {
+    //             val ossrhUsername: String by project
+    //             val ossrhPassword: String by project
+    //
+    //             username = ossrhUsername
+    //             password = ossrhPassword
+    //         }
+    //     }
+    // }
 }
 
-signing {
-    sign(publishing.publications["ossrh"])
-}
+// signing {
+//     sign(publishing.publications["ossrh"])
+// }
